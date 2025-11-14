c8df6c0ab8d0ed98edf37a8cab50fdc10fc533da
PeterParkers007 <3427463164@qq.com>
2025-11-14 13:39:10 +0800

    新增阵营关系预览窗口

--- Editor/FactionMemberEditor.cs
+++ Editor/FactionMemberEditor.cs
@@ -13,19 +13,31 @@ namespace TechCosmos.FactionForge.Editor
         private SerializedProperty factionNameProperty;
         private SerializedProperty canChangeFactionProperty;
 
+        // 缓存阵营列表和名称
+        private string[] factionNameArray = new string[0];
+        private int selectedFactionIndex = 0;
+        private bool showCustomInput = false;
+
         private void OnEnable()
         {
             factionMember = (FactionMember)target;
             factionNameProperty = serializedObject.FindProperty("factionName");
             canChangeFactionProperty = serializedObject.FindProperty("canChangeFaction");
+
+            RefreshFactionList();
         }
 
         public override void OnInspectorGUI()
         {
             serializedObject.Update();
 
-            // 1. 显示基础属性
-            EditorGUILayout.PropertyField(factionNameProperty);
+            EditorGUILayout.Space(5);
+            EditorGUILayout.LabelField("阵营配置", EditorStyles.boldLabel);
+
+            // 1. 智能阵营选择器
+            DrawSmartFactionSelector();
+
+            EditorGUILayout.Space(5);
             EditorGUILayout.PropertyField(canChangeFactionProperty);
 
             EditorGUILayout.Space(10);
@@ -41,91 +53,335 @@ namespace TechCosmos.FactionForge.Editor
             serializedObject.ApplyModifiedProperties();
         }
 
+        private void DrawSmartFactionSelector()
+        {
+            FactionManager manager = FindFactionManager();
+
+            EditorGUILayout.BeginVertical("box");
+            {
+                EditorGUILayout.LabelField("阵营选择", EditorStyles.miniBoldLabel);
+
+                if (manager == null || factionNameArray.Length == 0)
+                {
+                    // 没有FactionManager时的回退方案
+                    EditorGUILayout.PropertyField(factionNameProperty, new GUIContent("阵营名称"));
+                    EditorGUILayout.HelpBox("暂无可用阵营配置", MessageType.Info);
+                }
+                else
+                {
+                    // 刷新选择索引
+                    RefreshSelectedIndex();
+
+                    EditorGUILayout.BeginHorizontal();
+                    {
+                        // 阵营下拉选择器
+                        int newIndex = EditorGUILayout.Popup("选择阵营", selectedFactionIndex, factionNameArray);
+
+                        if (newIndex != selectedFactionIndex)
+                        {
+                            selectedFactionIndex = newIndex;
+                            factionNameProperty.stringValue = factionNameArray[newIndex];
+                        }
+
+                        // 刷新按钮
+                        if (GUILayout.Button("刷新", GUILayout.Width(50)))
+                        {
+                            RefreshFactionList();
+                            RefreshSelectedIndex();
+                        }
+                    }
+                    EditorGUILayout.EndHorizontal();
+
+                    // 自定义输入开关
+                    EditorGUILayout.BeginHorizontal();
+                    {
+                        showCustomInput = EditorGUILayout.Toggle("自定义输入", showCustomInput, GUILayout.Width(100));
+
+                        if (showCustomInput)
+                        {
+                            EditorGUILayout.PropertyField(factionNameProperty, GUIContent.none);
+                        }
+                        else
+                        {
+                            EditorGUILayout.LabelField($"当前: {factionNameProperty.stringValue}", EditorStyles.miniLabel);
+                        }
+                    }
+                    EditorGUILayout.EndHorizontal();
+
+                    // 显示阵营统计
+                    EditorGUILayout.LabelField($"已配置 {factionNameArray.Length} 个阵营", EditorStyles.miniLabel);
+                }
+            }
+            EditorGUILayout.EndVertical();
+        }
+
         private void DrawRelationshipPreview()
         {
             EditorGUILayout.LabelField("阵营关系预览", EditorStyles.boldLabel);
 
-            // 在编辑器模式下查找 FactionManager
             FactionManager manager = FindFactionManager();
 
             if (manager == null)
             {
                 EditorGUILayout.HelpBox("场景中未找到 FactionManager", MessageType.Warning);
 
-                // 添加创建按钮
                 if (GUILayout.Button("创建 FactionManager"))
                 {
                     CreateFactionManager();
+                    RefreshFactionList();
                 }
                 return;
             }
 
             if (string.IsNullOrEmpty(factionMember.FactionName))
             {
-                EditorGUILayout.HelpBox("请先设置阵营名称", MessageType.Info);
+                EditorGUILayout.HelpBox("请先选择阵营", MessageType.Info);
                 return;
             }
 
-            // 显示与其他所有阵营的关系
-            foreach (var faction in manager.factions)
+            EditorGUILayout.BeginVertical("box");
             {
-                if (faction.factionName == factionMember.FactionName) continue;
+                bool hasRelationships = false;
 
-                var relationship = manager.GetRelationship(
-                    factionMember.FactionName, faction.factionName);
-
-                EditorGUILayout.BeginHorizontal();
+                foreach (var faction in manager.factions)
                 {
-                    EditorGUILayout.LabelField(faction.factionName, GUILayout.Width(80));
+                    if (faction.factionName == factionMember.FactionName) continue;
 
-                    var style = new GUIStyle(EditorStyles.label);
-                    style.normal.textColor = GetRelationshipColor(relationship);
-                    EditorGUILayout.LabelField(relationship.ToString(), style);
+                    var relationship = manager.GetRelationship(factionMember.FactionName, faction.factionName);
+
+                    EditorGUILayout.BeginHorizontal();
+                    {
+                        // 阵营名称和关系状态
+                        EditorGUILayout.LabelField(faction.factionName, GUILayout.Width(80));
+
+                        // 关系颜色标签
+                        var style = new GUIStyle(EditorStyles.miniLabel);
+                        style.normal.textColor = GetRelationshipColor(relationship);
+                        style.alignment = TextAnchor.MiddleCenter;
+                        style.padding = new RectOffset(4, 4, 2, 2);
+
+                        EditorGUILayout.LabelField(relationship.ToString(), style, GUILayout.Width(60));
+
+                        // 快速关系设置按钮
+                        if (GUILayout.Button("...", GUILayout.Width(20)))
+                        {
+                            ShowRelationshipQuickMenu(faction.factionName, relationship);
+                        }
+                    }
+                    EditorGUILayout.EndHorizontal();
+
+                    hasRelationships = true;
                 }
-                EditorGUILayout.EndHorizontal();
+
+                if (!hasRelationships)
+                {
+                    EditorGUILayout.HelpBox("这是唯一的阵营，暂无其他关系", MessageType.Info);
+                }
             }
+            EditorGUILayout.EndVertical();
         }
 
         private void DrawQuickActions()
         {
             EditorGUILayout.LabelField("快速操作", EditorStyles.boldLabel);
 
-            EditorGUILayout.BeginHorizontal();
+            EditorGUILayout.BeginVertical("box");
             {
-                // 快速测试按钮
-                if (GUILayout.Button("测试关系"))
+                EditorGUILayout.BeginHorizontal();
                 {
-                    TestAllRelationships();
-                }
+                    if (GUILayout.Button("测试关系"))
+                    {
+                        TestAllRelationships();
+                    }
 
-                // 在场景中定位FactionManager
-                FactionManager manager = FindFactionManager();
-                if (manager != null && GUILayout.Button("定位管理器"))
-                {
-                    Selection.activeObject = manager.gameObject;
+                    FactionManager manager = FindFactionManager();
+                    if (manager != null && GUILayout.Button("打开管理器"))
+                    {
+                        Selection.activeObject = manager.gameObject;
+                    }
                 }
+                EditorGUILayout.EndHorizontal();
+
+                EditorGUILayout.BeginHorizontal();
+                {
+                    if (GUILayout.Button("刷新列表"))
+                    {
+                        RefreshFactionList();
+                        RefreshSelectedIndex();
+                    }
+
+                    if (GUILayout.Button("创建新阵营"))
+                    {
+                        ShowCreateFactionDialog();
+                    }
+                }
+                EditorGUILayout.EndHorizontal();
             }
-            EditorGUILayout.EndHorizontal();
+            EditorGUILayout.EndVertical();
         }
 
-        // 在编辑器模式下查找 FactionManager
+        private void RefreshFactionList()
+        {
+            FactionManager manager = FindFactionManager();
+            if (manager != null && manager.factions.Count > 0)
+            {
+                var names = new List<string>();
+                foreach (var faction in manager.factions)
+                {
+                    if (!string.IsNullOrEmpty(faction.factionName))
+                    {
+                        names.Add(faction.factionName);
+                    }
+                }
+                factionNameArray = names.ToArray();
+            }
+            else
+            {
+                factionNameArray = new string[0];
+            }
+        }
+
+        private void RefreshSelectedIndex()
+        {
+            selectedFactionIndex = 0;
+            for (int i = 0; i < factionNameArray.Length; i++)
+            {
+                if (factionNameArray[i] == factionNameProperty.stringValue)
+                {
+                    selectedFactionIndex = i;
+                    break;
+                }
+            }
+        }
+
+        private void ShowRelationshipQuickMenu(string targetFaction, FactionRelationship currentRelationship)
+        {
+            GenericMenu menu = new GenericMenu();
+
+            menu.AddItem(new GUIContent("设为敌对"), currentRelationship == FactionRelationship.Hostile,
+                () => SetRelationship(targetFaction, FactionRelationship.Hostile));
+            menu.AddItem(new GUIContent("设为中立"), currentRelationship == FactionRelationship.Neutral,
+                () => SetRelationship(targetFaction, FactionRelationship.Neutral));
+            menu.AddItem(new GUIContent("设为友好"), currentRelationship == FactionRelationship.Friendly,
+                () => SetRelationship(targetFaction, FactionRelationship.Friendly));
+            menu.AddItem(new GUIContent("设为同盟"), currentRelationship == FactionRelationship.Allied,
+                () => SetRelationship(targetFaction, FactionRelationship.Allied));
+
+            menu.ShowAsContext();
+        }
+
+        private void SetRelationship(string targetFaction, FactionRelationship relationship)
+        {
+            FactionManager manager = FindFactionManager();
+            if (manager != null)
+            {
+                manager.SetRelationship(factionMember.FactionName, targetFaction, relationship);
+                EditorUtility.SetDirty(manager);
+                Debug.Log($"已设置 {factionMember.FactionName} 与 {targetFaction} 的关系为 {relationship}");
+            }
+        }
+
+        private void ShowCreateFactionDialog()
+        {
+            FactionManager manager = FindFactionManager();
+            if (manager == null)
+            {
+                manager = CreateFactionManager();
+            }
+
+            // 简单的新建阵营对话框
+            string newFactionName = EditorInputDialog.Show("新建阵营", "请输入新阵营名称:", "NewFaction");
+            if (!string.IsNullOrEmpty(newFactionName))
+            {
+                // 检查是否已存在
+                bool exists = false;
+                foreach (var faction in manager.factions)
+                {
+                    if (faction.factionName == newFactionName)
+                    {
+                        exists = true;
+                        break;
+                    }
+                }
+
+                if (!exists)
+                {
+                    manager.factions.Add(new Faction { factionName = newFactionName });
+                    manager.RefreshAllRelationships();
+                    EditorUtility.SetDirty(manager);
+                    RefreshFactionList();
+
+                    // 自动选择新创建的阵营
+                    factionNameProperty.stringValue = newFactionName;
+                    RefreshSelectedIndex();
+
+                    Debug.Log($"已创建新阵营: {newFactionName}");
+                }
+                else
+                {
+                    EditorUtility.DisplayDialog("错误", $"阵营 '{newFactionName}' 已存在!", "确定");
+                }
+            }
+        }
+
+        // 需要添加这个输入对话框类
+        private class EditorInputDialog : EditorWindow
+        {
+            private string inputText = "";
+            private System.Action<string> onConfirm;
+            private string message;
+
+            public static string Show(string title, string message, string defaultValue = "")
+            {
+                EditorInputDialog window = CreateInstance<EditorInputDialog>();
+                window.titleContent = new GUIContent(title);
+                window.message = message;
+                window.inputText = defaultValue;
+                window.minSize = new Vector2(300, 120);
+                window.maxSize = new Vector2(300, 120);
+                window.ShowModal();
+                return window.inputText;
+            }
+
+            private void OnGUI()
+            {
+                GUILayout.Space(10);
+                EditorGUILayout.LabelField(message);
+                GUILayout.Space(10);
+
+                inputText = EditorGUILayout.TextField(inputText);
+
+                GUILayout.Space(20);
+                EditorGUILayout.BeginHorizontal();
+                {
+                    if (GUILayout.Button("确定"))
+                    {
+                        Close();
+                    }
+                    if (GUILayout.Button("取消"))
+                    {
+                        inputText = "";
+                        Close();
+                    }
+                }
+                EditorGUILayout.EndHorizontal();
+            }
+        }
+
+        // 原有的辅助方法保持不变
         private FactionManager FindFactionManager()
         {
-            // 方法1: 先尝试通过单例获取（运行时）
             if (FactionManager.Instance != null)
                 return FactionManager.Instance;
-
-            // 方法2: 在场景中查找（编辑时）
             return FindObjectOfType<FactionManager>();
         }
 
-        // 创建 FactionManager
-        private void CreateFactionManager()
+        private FactionManager CreateFactionManager()
         {
             var managerObj = new GameObject("FactionManager");
-            managerObj.AddComponent<FactionManager>();
+            var manager = managerObj.AddComponent<FactionManager>();
             EditorUtility.SetDirty(managerObj);
-            Debug.Log("已创建 FactionManager");
+            return manager;
         }
 
         private Color GetRelationshipColor(FactionRelationship relationship)
@@ -155,8 +411,7 @@ namespace TechCosmos.FactionForge.Editor
             {
                 if (faction.factionName != factionMember.FactionName)
                 {
-                    var rel = manager.GetRelationship(
-                        factionMember.FactionName, faction.factionName);
+                    var rel = manager.GetRelationship(factionMember.FactionName, faction.factionName);
                     Debug.Log($"{factionMember.FactionName} ←→ {faction.factionName} : {rel}");
                 }
             }
---
+++ Editor/FactionRelationshipWindow.cs
@@ -0,0 +1,290 @@
+#if UNITY_EDITOR
+using UnityEditor;
+using UnityEngine;
+using System.Collections.Generic;
+using TechCosmos.FactionForge.Runtime;
+
+namespace TechCosmos.FactionForge.Editor
+{
+    public class FactionRelationshipWindow : EditorWindow
+    {
+        private Vector2 scrollPosition;
+        private bool autoRefresh = true;
+        private float lastRefreshTime;
+        private const float REFRESH_INTERVAL = 1.0f; // 每秒自动刷新一次
+
+        [MenuItem("Tools/FactionForge/阵营关系可视化 &f")]
+        public static void ShowWindow()
+        {
+            var window = GetWindow<FactionRelationshipWindow>("阵营关系");
+            window.minSize = new Vector2(400, 500);
+            window.Show();
+        }
+
+        private void OnEnable()
+        {
+            RefreshData();
+        }
+
+        private void OnGUI()
+        {
+            DrawToolbar();
+
+            EditorGUILayout.Space();
+
+            scrollPosition = EditorGUILayout.BeginScrollView(scrollPosition);
+            {
+                DrawRelationshipMatrix();
+                DrawStatistics();
+            }
+            EditorGUILayout.EndScrollView();
+
+            // 自动刷新
+            if (autoRefresh && EditorApplication.timeSinceStartup - lastRefreshTime > REFRESH_INTERVAL)
+            {
+                RefreshData();
+                Repaint();
+            }
+        }
+
+        private void DrawToolbar()
+        {
+            EditorGUILayout.BeginHorizontal(EditorStyles.toolbar);
+            {
+                // 标题
+                EditorGUILayout.LabelField("阵营关系总览", EditorStyles.boldLabel);
+
+                GUILayout.FlexibleSpace();
+
+                // 自动刷新开关
+                autoRefresh = GUILayout.Toggle(autoRefresh, "自动刷新", EditorStyles.toolbarButton);
+
+                // 手动刷新按钮
+                if (GUILayout.Button("刷新", EditorStyles.toolbarButton))
+                {
+                    RefreshData();
+                }
+
+                // 打开管理器按钮
+                if (GUILayout.Button("打开管理器", EditorStyles.toolbarButton))
+                {
+                    Selection.activeObject = FindFactionManager()?.gameObject;
+                }
+
+                // 创建管理器按钮
+                if (GUILayout.Button("创建管理器", EditorStyles.toolbarButton))
+                {
+                    CreateFactionManager();
+                    RefreshData();
+                }
+            }
+            EditorGUILayout.EndHorizontal();
+        }
+
+        private void DrawRelationshipMatrix()
+        {
+            var manager = FindFactionManager();
+
+            if (manager == null || manager.factions.Count == 0)
+            {
+                EditorGUILayout.HelpBox("未找到阵营数据\n请先创建 FactionManager 并添加阵营", MessageType.Info);
+                return;
+            }
+
+            EditorGUILayout.LabelField("关系矩阵", EditorStyles.boldLabel);
+            EditorGUILayout.Space(5);
+
+            // 表头
+            EditorGUILayout.BeginHorizontal();
+            {
+                EditorGUILayout.LabelField("阵营", EditorStyles.boldLabel, GUILayout.Width(120));
+                foreach (var faction in manager.factions)
+                {
+                    EditorGUILayout.LabelField(faction.factionName, EditorStyles.boldLabel, GUILayout.Width(80));
+                }
+            }
+            EditorGUILayout.EndHorizontal();
+
+            EditorGUILayout.Space(5);
+
+            // 关系矩阵
+            for (int i = 0; i < manager.factions.Count; i++)
+            {
+                var currentFaction = manager.factions[i];
+
+                EditorGUILayout.BeginHorizontal();
+                {
+                    // 行头 - 阵营名称
+                    EditorGUILayout.LabelField(currentFaction.factionName, GUILayout.Width(120));
+
+                    // 关系单元格
+                    for (int j = 0; j < manager.factions.Count; j++)
+                    {
+                        if (i == j)
+                        {
+                            // 对角线 - 显示为自身
+                            EditorGUILayout.LabelField("自身", GetCellStyle(FactionRelationship.Neutral), GUILayout.Width(80));
+                        }
+                        else
+                        {
+                            var otherFaction = manager.factions[j];
+                            var relationship = manager.GetRelationship(currentFaction.factionName, otherFaction.factionName);
+
+                            if (GUILayout.Button(relationship.ToString(), GetCellStyle(relationship), GUILayout.Width(80)))
+                            {
+                                // 点击单元格快速修改关系
+                                ShowRelationshipQuickMenu(currentFaction.factionName, otherFaction.factionName, relationship);
+                            }
+                        }
+                    }
+                }
+                EditorGUILayout.EndHorizontal();
+
+                // 行间隔
+                if (i < manager.factions.Count - 1)
+                {
+                    EditorGUILayout.Space(2);
+                }
+            }
+        }
+
+        private void DrawStatistics()
+        {
+            var manager = FindFactionManager();
+            if (manager == null || manager.factions.Count == 0) return;
+
+            EditorGUILayout.Space(15);
+            EditorGUILayout.LabelField("统计信息", EditorStyles.boldLabel);
+
+            EditorGUILayout.BeginVertical("box");
+            {
+                EditorGUILayout.LabelField($"总阵营数量: {manager.factions.Count}");
+
+                // 统计各种关系的数量
+                int hostileCount = 0, friendlyCount = 0, alliedCount = 0, neutralCount = 0;
+
+                for (int i = 0; i < manager.factions.Count; i++)
+                {
+                    for (int j = i + 1; j < manager.factions.Count; j++)
+                    {
+                        var rel = manager.GetRelationship(
+                            manager.factions[i].factionName,
+                            manager.factions[j].factionName
+                        );
+
+                        switch (rel)
+                        {
+                            case FactionRelationship.Hostile: hostileCount++; break;
+                            case FactionRelationship.Friendly: friendlyCount++; break;
+                            case FactionRelationship.Allied: alliedCount++; break;
+                            case FactionRelationship.Neutral: neutralCount++; break;
+                        }
+                    }
+                }
+
+                EditorGUILayout.BeginHorizontal();
+                {
+                    EditorGUILayout.LabelField($"敌对关系: {hostileCount}", GetMiniLabelStyle(Color.red));
+                    EditorGUILayout.LabelField($"友好关系: {friendlyCount}", GetMiniLabelStyle(Color.green));
+                }
+                EditorGUILayout.EndHorizontal();
+
+                EditorGUILayout.BeginHorizontal();
+                {
+                    EditorGUILayout.LabelField($"同盟关系: {alliedCount}", GetMiniLabelStyle(Color.cyan));
+                    EditorGUILayout.LabelField($"中立关系: {neutralCount}", GetMiniLabelStyle(Color.gray));
+                }
+                EditorGUILayout.EndHorizontal();
+            }
+            EditorGUILayout.EndVertical();
+        }
+
+        private GUIStyle GetCellStyle(FactionRelationship relationship)
+        {
+            var style = new GUIStyle(EditorStyles.miniButton);
+
+            switch (relationship)
+            {
+                case FactionRelationship.Hostile:
+                    style.normal.textColor = Color.red;
+                    break;
+                case FactionRelationship.Friendly:
+                    style.normal.textColor = Color.green;
+                    break;
+                case FactionRelationship.Allied:
+                    style.normal.textColor = Color.cyan;
+                    break;
+                case FactionRelationship.Neutral:
+                    style.normal.textColor = Color.gray;
+                    break;
+            }
+
+            return style;
+        }
+
+        private GUIStyle GetMiniLabelStyle(Color color)
+        {
+            var style = new GUIStyle(EditorStyles.miniLabel);
+            style.normal.textColor = color;
+            return style;
+        }
+
+        private void ShowRelationshipQuickMenu(string factionA, string factionB, FactionRelationship currentRelationship)
+        {
+            GenericMenu menu = new GenericMenu();
+
+            menu.AddItem(new GUIContent($"设为敌对"), currentRelationship == FactionRelationship.Hostile,
+                () => SetRelationship(factionA, factionB, FactionRelationship.Hostile));
+            menu.AddItem(new GUIContent($"设为中立"), currentRelationship == FactionRelationship.Neutral,
+                () => SetRelationship(factionA, factionB, FactionRelationship.Neutral));
+            menu.AddItem(new GUIContent($"设为友好"), currentRelationship == FactionRelationship.Friendly,
+                () => SetRelationship(factionA, factionB, FactionRelationship.Friendly));
+            menu.AddItem(new GUIContent($"设为同盟"), currentRelationship == FactionRelationship.Allied,
+                () => SetRelationship(factionA, factionB, FactionRelationship.Allied));
+
+            menu.ShowAsContext();
+        }
+
+        private void SetRelationship(string factionA, string factionB, FactionRelationship relationship)
+        {
+            var manager = FindFactionManager();
+            if (manager != null)
+            {
+                manager.SetRelationship(factionA, factionB, relationship);
+                EditorUtility.SetDirty(manager);
+                RefreshData();
+                Debug.Log($"已设置 {factionA} 与 {factionB} 的关系为 {relationship}");
+            }
+        }
+
+        private void RefreshData()
+        {
+            lastRefreshTime = (float)EditorApplication.timeSinceStartup;
+        }
+
+        private FactionManager FindFactionManager()
+        {
+            if (FactionManager.Instance != null)
+                return FactionManager.Instance;
+            return FindObjectOfType<FactionManager>();
+        }
+
+        private void CreateFactionManager()
+        {
+            var managerObj = new GameObject("FactionManager");
+            managerObj.AddComponent<FactionManager>();
+            EditorUtility.SetDirty(managerObj);
+            Debug.Log("已创建 FactionManager");
+        }
+
+        // 在Project窗口中右键创建菜单
+        [MenuItem("GameObject/Tools/FactionForge/创建阵营管理器", false, 0)]
+        static void CreateFactionManagerMenu()
+        {
+            var managerObj = new GameObject("FactionManager");
+            managerObj.AddComponent<FactionManager>();
+            Selection.activeObject = managerObj;
+        }
+    }
+}
+#endif
